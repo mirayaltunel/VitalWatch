1487340f1110cb7dc4cdd22bb186021a871c8acb
mirayaltunel <mirayaltunel@gmail.com>
2026-05-03 17:53:49 +0300

    Add project files.

---
+++ VitalWatch.Api/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VitalWatch.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public UserController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("test");
+        }
+    }
+}
